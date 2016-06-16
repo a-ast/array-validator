27a986ff6499cb434e42476b74c59f65e8d46d43
aa <andrey.astakhov@gmail.com>
2016-06-16 22:23:03 +0200

    Added simple test for SimpleXPathMatcher.

--- tests/SimpleXPathMatcherTest.php
+++ tests/SimpleXPathMatcherTest.php
@@ -2,9 +2,13 @@
 
 namespace Aa\ArrayValidator\Tests;
 
+use Aa\ArrayValidator\SimpleXPathMatcher;
 use PHPUnit_Framework_TestCase;
 
 class SimpleXPathMatcherTest extends PHPUnit_Framework_TestCase
 {
-
+    public function testConstructor()
+    {
+        new SimpleXPathMatcher();
+    }
 }
